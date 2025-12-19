ddd13eee008dc380df3ebe82af9ffaacc764ffcd
Dev Raghuwanshi <devraghuwanshi18@gmail.com>
2025-12-19 23:48:06 +0530

    Add random color change functionality

--- Projects/Color scheme switcher/script.js
+++ Projects/Color scheme switcher/script.js
@@ -2,12 +2,14 @@ let grey = document.getElementsByClassName("grey")[0];
 let white = document.getElementsByClassName("white")[0];
 let black = document.getElementsByClassName("black")[0];
 let yellow = document.getElementsByClassName("yellow")[0];
+let random = document.getElementsByClassName("random")[0];
 let body = document.body;
 
 grey.addEventListener("click", changeGrey);
 white.addEventListener("click", changeWhite);
 black.addEventListener("click", changeBlack);
 yellow.addEventListener("click", changeYellow);
+random.addEventListener("click" , changeRandom);
 
 function changeGrey() {
   body.style.backgroundColor = "grey";
@@ -22,4 +24,10 @@ function changeBlack() {
 }
 function changeYellow() {
   body.style.backgroundColor = "yellow";
+}
+function changeRandom(){
+  let r = Math.floor(Math.random()*256);
+  let g = Math.floor(Math.random()*256);
+  let b = Math.floor(Math.random()*256);
+  body.style.backgroundColor = `rgb(${r}, ${g}, ${b} )`;
 }
