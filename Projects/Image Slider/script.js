856e567c44639aeba43ddd2049560f01e5f5fd7a
Dev Raghuwanshi <devraghuwanshi18@gmail.com>
2026-04-05 23:33:08 +0530

    Add Image Slider
    A horizontal image slider built with JavaScript that allows smooth navigation using mouse scroll and button controls.

---
+++ Projects/Image Slider/script.js
@@ -0,0 +1,19 @@
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.querySelector("#backBtn");
+let nextBtn = document.querySelector("#nextBtn");
+
+
+scrollContainer.addEventListener("wheel" , (e)=>{
+    e.preventDefault(); 
+    scrollContainer.scrollLeft += e.deltaY;
+});
+
+nextBtn.addEventListener("click" , ()=>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 900;
+});
+
+backBtn.addEventListener("click", () => {
+  scrollContainer.scrollLeft -= 900;
+  scrollContainer.style.scrollBehavior = "smooth";
+});
